78e0b0429c1dfe0e00c77ca01f2ae8380f53bf52
spade-as-in-ace <73505788+spade-as-in-ace@users.noreply.github.com>
2022-06-02 13:17:30 -0400

    Update time_logger.py

--- scripts/time_logger.py
+++ scripts/time_logger.py
@@ -1,56 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Basic Time-Logging Script
-v. 0.0.3
+Basic Time-Logging Script for Windows
+v. 0.0.7
 """
 from datetime import datetime as dt
-from os import system
-from os.path import expanduser
+from os import mkdir, system
+from os.path import exists, expanduser
 
-LOG_PATH = f"{expanduser('~')}\\Documents\\Logs"
-KEYS = {"b": "break", "l": "lunch", "r": "return", "m": "message", "s": "shutdown"}
+DATE_FORMAT = "%d-%m-%Y"
+TIME_FORMAT = "%H:%M:%S"
+DELIMITER = ","
+
+LOG_PATH = f"{expanduser('~')}\\Documents\\logs"
+KEYS = {'b': "break", 'l': "lunch", 'r': "return", 'm': "message", 'q': "quit", 's': "shutdown"}
 
 
 def main():
     global LOG_PATH
+    if not exists(LOG_PATH):
+        mkdir(LOG_PATH)
     time = dt.now()
-    file_path = f"{LOG_PATH}\\{time.strftime('%d-%m-%Y')}.csv"
-    log_file = open(file_path, "a")
-    log_file.write(f"startup, {time.strftime('%H:%M:%S')}, \n")
+    file_path = f"{LOG_PATH}\\{time.strftime(DATE_FORMAT)}.csv"
+    log_file = open(file_path, 'a')
+    log_file.write(f"startup{DELIMITER}{time.strftime(TIME_FORMAT)}\n")
     log_file.close()
 
     while True:
         key = input("> ").split(maxsplit=1)
         time = dt.now()
-        log_file = open(file_path, "a")
+        log_file = open(file_path, 'a')
 
         if time.strftime("%d-%m-%Y") != log_file.name[-14:-4]:
             log_file.close()
             file_path = f"{LOG_PATH}\\{time.strftime('%d-%m-%Y')}.csv"
-            log_file = open(file_path, "a")
+            log_file = open(file_path, 'a')
 
         time = time.strftime("%H:%M:%S")
 
-        if key[0] in KEYS.keys():
-            if key[0] == "s":
-                if input("confirm shutdown > ") == "s":
+        if key[0] in KEYS:
+            if key[0] == 's':
+                if input("confirm shutdown > ") == 's':
                     system("shutdown /s /t 30")
                 else:
                     print("shutdown aborted")
                     continue
 
-            log_file.write(f"{KEYS[key[0]]}, {time},")
-
+            log_file.write(f"{KEYS[key[0]]}{DELIMITER}{time}")
             if len(key) == 2:
-                log_file.write(f" {key[1]}")
-
+                log_file.write(f"{DELIMITER}{key[1]}")
             log_file.write("\n")
+
+            if key[0] == 'q':
+                return
             print(f"{KEYS[key[0]]} logged {time}")
 
         else:
-            print(f"Key '{key[0]}' unrocognized")
+            print(f"Key '{key[0]}' unrecognized")
         log_file.close()
 
-
-main()
+if __name__ == "__main__":
+    main()
